423dbec7f47ec4a3a89705e0295d411264f07e0a
sarafat27 <sarafat2769@gmail.com>
2022-03-19 02:16:26 +0600

    key added

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
-
+import './Countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([])
 
@@ -14,9 +14,11 @@ const Countries = () => {
     return (
         <div>
             <h1>Going to travel different countries: {countries.length}</h1>
-            {
-                countries.map(country => <Country country={country}></Country>)
-            }
+            <div className='countries'>
+                {
+                    countries.map(country => <Country country={country} key={country.cca3}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Country.css'
 const Country = (props) => {
-    console.log(props)
-    const { name, capital, area, region } = props.country
+    const { name, capital, area, region, flags } = props.country
     return (
         <div className='country'>
             <h2 style={{ backgroundColor: 'black', padding: '10px' }}>Country name: {name.common}</h2>
             <h4>Capital: {capital}</h4>
             <h6>area: {area}</h6>
             <h6>Region: {region}</h6>
-        </div>
+            <img src={flags.png} alt="" />
+        </div >
     );
 };
 
